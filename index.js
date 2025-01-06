293b070f91e76166c11ca9aabc6811801d6ca836
NureAlam68 <nurealam151068@gmail.com>
2025-01-06 12:09:23 +0600

    get data using query

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ async function run() {
 
     const menuCollection = client.db("MealsDB").collection("menu");
     const reviewCollection = client.db("MealsDB").collection("reviews");
+    const cartCollection = client.db("MealsDB").collection("carts");
 
     app.get("/menu", async(req, res) => {
         const result = await menuCollection.find().toArray();
@@ -36,6 +37,21 @@ async function run() {
     app.get("/reviews", async(req, res) => {
         const result = await reviewCollection.find().toArray();
         res.send(result);
+    })
+
+    // carts collection
+    app.get('/carts', async(req, res) => {
+      const email = req.query.email;
+      const query = { email: email }
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    
+    app.post('/carts', async(req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
     })
 
     // Send a ping to confirm a successful connection
