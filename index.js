02a366122b7f874943c7092ec5722a36455f53cb
NureAlam68 <nurealam151068@gmail.com>
2025-01-12 15:44:15 +0600

    patch and delete user

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ async function run() {
       const result = await userCollection.find().toArray();
       res.send(result);
     })
+
     app.post('/users', async(req, res) => {
       const user = req.body;
       // insert email if user does not exists:
@@ -49,6 +50,26 @@ async function run() {
       res.send(result);
     })
 
+    app.patch('/users/admin/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id)};
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
+
+    app.delete('/users/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    // menu related apis
     app.get("/menu", async(req, res) => {
         const result = await menuCollection.find().toArray();
         res.send(result);
